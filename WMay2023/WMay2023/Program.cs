d84074c2f53b6c069bba9c03d25fe1016d06cdd1
galenyjl <galenyjl@gmail.com>
2023-05-20 13:07:30 +1200

    Until Add new button

--- WMay2023/WMay2023/Program.cs
+++ WMay2023/WMay2023/Program.cs
@@ -19,7 +19,7 @@ passwordTextbox.SendKeys("string2");
 // Identify login button and click on login button
 IWebElement loginButton = driver.FindElement(By.XPath("//*[@id=\"normal_login\"]/div[3]/div/div/div/div/button"));
 loginButton.Click();
-Thread.Sleep(3000);
+Thread.Sleep(5000);
 
 // Check if user is logged in successfully
 IWebElement ReportMenu = driver.FindElement(By.XPath("//*[@id=\"root\"]/div/div[1]/div/div/div[2]/ul/li[6]/div/span[2]"));
@@ -33,5 +33,32 @@ else
     Console.WriteLine("Login failed, test failed");
 }
 
+// Create an employee
 
+// Go to employee page
+IWebElement threeDotButton = driver.FindElement(By.XPath("//*[@id=\"root\"]/div/div[1]/div/div/div[2]/ul/li[8]/div"));
+threeDotButton.Click();
+Thread.Sleep(1000);
+
+IWebElement settingsButton = driver.FindElement(By.XPath("/html/body/div[4]/div/div"));
+settingsButton.Click();
+Thread.Sleep(1000);
+
+IWebElement employeeManagementMenu = driver.FindElement(By.XPath("/html/body/div[5]/div/div/ul/li[1]"));
+employeeManagementMenu.Click();
+Thread.Sleep(2000);
+
+// Click on Add New Button
+IWebElement addNewButton = driver.FindElement(By.XPath("//*[@id=\"root\"]/div/div[2]/div/div[2]/div/div/div[1]/div[1]/div[2]/div/div/span/button"));
+addNewButton.Click();
+
+// Input First Name
+
+// Input Last Name
+
+// Input Username
+
+// Input Password
+
+// Click on Save button
 
