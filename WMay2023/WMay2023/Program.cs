3aae35f21e58a522c57840141c3316c558e0d396
galenyjl <galenyjl@gmail.com>
2023-05-21 19:36:47 +1200

    Create new employee added

--- WMay2023/WMay2023/Program.cs
+++ WMay2023/WMay2023/Program.cs
@@ -19,7 +19,7 @@ passwordTextbox.SendKeys("string2");
 // Identify login button and click on login button
 IWebElement loginButton = driver.FindElement(By.XPath("//*[@id=\"normal_login\"]/div[3]/div/div/div/div/button"));
 loginButton.Click();
-Thread.Sleep(5000);
+Thread.Sleep(6000);
 
 // Check if user is logged in successfully
 IWebElement ReportMenu = driver.FindElement(By.XPath("//*[@id=\"root\"]/div/div[1]/div/div/div[2]/ul/li[6]/div/span[2]"));
@@ -53,12 +53,39 @@ IWebElement addNewButton = driver.FindElement(By.XPath("//*[@id=\"root\"]/div/di
 addNewButton.Click();
 
 // Input First Name
+IWebElement firstNameTextbox = driver.FindElement(By.Id("firstName"));
+firstNameTextbox.SendKeys("Test");
 
 // Input Last Name
+IWebElement lastNameTextbox = driver.FindElement(By.Id("lastName"));
+lastNameTextbox.SendKeys("User");
 
 // Input Username
+IWebElement employeeUsernameTextbox = driver.FindElement(By.Id("userName"));
+employeeUsernameTextbox.SendKeys("TestUser");
 
 // Input Password
+IWebElement employeePasswordTextbox = driver.FindElement(By.Id("password"));
+employeePasswordTextbox.SendKeys("123123");
 
 // Click on Save button
+IWebElement saveButton = driver.FindElement(By.XPath("//*[@id=\"root\"]/div/div[2]/div/div[2]/div/div/div[2]/div[2]/div/div/div/table/tbody/tr[2]/td[5]/div/button[1]"));
+saveButton.Click();
+Thread.Sleep(3000);
 
+// Scroll down to the bottom
+IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+js.ExecuteScript("window.scrollTo(0, document.body.scrollHeight)");
+Thread.Sleep(1000);
+
+// Check if record created is present in the table and has expected value
+IWebElement actualUsername = driver.FindElement(By.XPath("//*[@id=\"root\"]/div/div[2]/div/div[2]/div/div/div[2]/div[2]/div/div/div/table/tbody/tr[last()]/td[1]/div/div[3]"));
+
+if (actualUsername.Text == "Username: TestUser")
+{
+    Console.WriteLine("Employee created successfully, test passed");
+}
+else
+{
+    Console.WriteLine("Test failed");
+}
